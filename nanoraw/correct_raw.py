0953ed0ac86f79b8463e63081872b3f5513a5424
Marcus Stoiber <marcus.stoiber@gmail.com>
2016-12-16 13:22:19 -0700

    bump version for manuscript code

--- nanoraw/correct_raw.py
+++ nanoraw/correct_raw.py
@@ -38,7 +38,7 @@ with os.fdopen(os.dup(fd), 'w') as old_stderr:
         _redirect_stderr(old_stderr)
 
 
-NANORAW_VERSION = '0.3'
+NANORAW_VERSION = '0.3.1'
 VERBOSE = False
 
 indelStats = namedtuple('indelStats',
