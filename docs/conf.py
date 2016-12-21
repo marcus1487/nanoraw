477a6bdfba37f845eb677149ff7ed087f1416f39
Marcus Stoiber <marcus.stoiber@gmail.com>
2016-12-21 15:52:31 -0800

    Fixed githubpages extension error on readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
